785b768e3d025ab87faebe3556a74463d248830c
helpermethod <oliver@sdkman.io>
2022-01-13 16:48:47 +0100

    Add formula

---
+++ Formula/sdkman.rb
@@ -0,0 +1,15 @@
+class Sdkman < Formula
+  desc "The SDKMAN! Command Line Interface"
+  homepage "https://sdkman.io"
+  url "https://github.com/sdkman/sdkman-cli/releases/download/5.13.1/sdkman-cli-5.13.1.zip"
+  sha256 "938bfd774e6c3b3564e0ed662189e683329675189d2ad7dd41d8d1ab02be0988"
+  license "Apache-2.0"
+
+  def install
+    libexec.install Dir["*"]
+  end
+
+  test do
+    
+  end
+end
